a79f6950606d9c99e4381f0c3e9305446b1593af
dtutila <daniel@dtutila.com>
2023-12-29 09:51:43 -0600

    added initial polybar configuration

---
+++ polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DIR="$HOME/.config/polybar"
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+#polybar -q main -c "$DIR"/config.ini &
+polybar  -q minimal-hdmi -c "$DIR"/minimal.ini &
+polybar  -q minimal-dp -c "$DIR"/minimal.ini &
+
