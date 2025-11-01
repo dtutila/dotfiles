9f5e9e49ed339d8097170f8ebe4d287e351fc77b
dtutila <daniel@dtutila.com>
2025-11-01 09:44:59 -0600

    sf-cli updated

--- bin/sf_install.sh
+++ bin/sf_install.sh
@@ -7,7 +7,7 @@
 wget "https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable/sf-linux-x64.tar.xz" -O sf.tar.gz
 
 # Extract files to ~/bin/sf directory
-mkdir -p ~/bin/sf
+mkdir -p ~/Applications/sf
 tar -xvf sf.tar.gz -C ~/bin/sf --strip-components 1; rm sf.tar.gz
 
 export PATH=~/bin/sf/bin:$PATH
